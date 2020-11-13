1709133c9b55c80af01e57d207d358f70b3f8065
Nina Mutty <nmutty@indeed.com>
2020-11-12 19:07:49 -0800

    Updates

---
+++ precache-manifest.a05873d7d2d68ee93eb3761e655174f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d27ba7d588698f11853ff70d6015f2",
+    "url": "/nina-mutty/index.html"
+  },
+  {
+    "revision": "ab28032453813f41aeaf",
+    "url": "/nina-mutty/static/css/main.da574bf2.chunk.css"
+  },
+  {
+    "revision": "bf70fa5279b7a8e90474",
+    "url": "/nina-mutty/static/js/2.19ed9be6.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/nina-mutty/static/js/2.19ed9be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab28032453813f41aeaf",
+    "url": "/nina-mutty/static/js/main.ca32756d.chunk.js"
+  },
+  {
+    "revision": "ce5d043dd8b38b4b0872",
+    "url": "/nina-mutty/static/js/runtime-main.c1cc19d6.js"
+  }
+]);
